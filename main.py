7796627556e28e2b1c1576f55bf8909ccb4703d8
Your Name <you@example.com>
2022-12-18 09:25:03 +0530

    pdf extraction

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+# STEP 1
+# import libraries
 import fitz
-file = fitz.open('report.pdf')
-#for pageNumber, page in enumerate(file.pages(), start=1):
-#    for imgNumber, img in enumerate(file.get_page_images(), start=1):
-image_list=file.get_page_images(1)
-for image in image_list:
-        xref = img[0]
-        pix = fitz.Pixmap(file, xref)
-        if pix.n > 4:
-            pix = fitz.Pixmap(fitz.csRGB, pix)
-        pix.writePNG(f'image_page{pageNumber}_{imgNumber}.png')
+fitz.restore_aliases()
+import io
+from PIL import Image
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGul as qtg
+from PyQt5 import QtCore as qtc
+
+# STEP 2
+# file path you want to extract images from
+file = "C:/Users/ashbi/Desktop/project/report.pdf"
+
+# open the file
+
+pdf_file= fitz.open(file)
+# enter the page number where ur desired image is
+n=int(input("enter the page number"))
+page= pdf_file[n]
+image_list = page.get_images()
+ln=len(image_list)
+#we get the number of images in that page
+if image_list:
+    		print(f"Number of images= {ln}")
+else:
+		print(" No images found on page")
+#To see those images and save from those	
